f71b70864bb7496b1a7e0caa57c6efadce098263
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-10-12 14:20:34 +0300

    Update scrollup.js

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -1,5 +1,10 @@
 export const scrollUpBtn = document.querySelector('.btn-scroll-up');
 
 export function onBtnScrollUp() {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
 };
