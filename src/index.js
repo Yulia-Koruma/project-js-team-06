13162e3e75db45d3c0fbe9787966be274c6d03b9
Korobenko2023 <vivika3110@gmail.com>
2023-10-14 20:05:02 +0300

    add mob html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './js/support_object.js';
 import './js/scrollup';
-import './js/header';
+
 import './js/support_object.js';
 import './js/scrollup';
 import './js/bookcard.js';
 import './js/shop-list';
+import './js/mobil-menu.js';
 
 
