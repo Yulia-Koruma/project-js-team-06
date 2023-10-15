be132466d49e04a6916cb59f50930d679c2f5647
JarPr <131611487+JarPr@users.noreply.github.com>
2023-10-15 03:21:50 +0200

    bookshelf-7

--- src/js/bookcard.js
+++ src/js/bookcard.js
@@ -1,8 +1,37 @@
 import "./category"
 import { fetchTopBooks } from "./api"
+import throttle from 'lodash.throttle';
 
 const bookGallery = document.querySelector('.bookgallery');
-const homeBooksByType = document.querySelector('.home-typeBook')
+let isActive;
+let currentRenderWidth = 375;
+let reloadState = true;
+
+window.addEventListener('resize', throttle(onResizewindow, 200));
+
+function onResizewindow() {
+  isActive = bookGallery.classList.contains('container_active');
+  if (!isActive) return;
+  if (
+    (window.innerWidth > 767 && currentRenderWidth < 768) ||
+    (window.innerWidth > 1439 && currentRenderWidth < 1440) ||
+    (window.innerWidth < 1440 && currentRenderWidth > 1439) ||
+    (window.innerWidth < 768 && currentRenderWidth > 767)
+  ) {
+    location.reload();
+  }
+}
+
+currentRenderWidth = window.innerWidth;
+let amountRenderedBooks = 1;
+if (currentRenderWidth < 768) {
+  amountRenderedBooks = 1;
+} else if (currentRenderWidth > 767 && currentRenderWidth < 1440) {
+  amountRenderedBooks = 3;
+} else {
+  amountRenderedBooks = 5;
+}
+
 
 const createTopBooksMarkup = async () => {
    let markup = await fetchTopBooks();
@@ -30,13 +59,11 @@ function booksCardTemplate(data) {
                   <a href="#" class="books-card-link" aria-label="books-item-link" rel="noopener noreferrer" data-id='${book._id}'>
                      <div class="books-card">
                          <img src="${book.book_image}" alt="${book.title}" class="books-card-img" width="180" height="256"/>
-                         <div class="books-overlay">
-                           <p class="books-overlay-text">quick view</p>
-                         </div>
+                         <p class="books-overlay-text">quick view</p>
                      </div> 
                      <div class="books-descr">
-                         <h3 class="books-card-title">${book.title}</h3>
-                         <p class="books-card-author">${book.author}</p>
+                         <h3 class="books-card-title">${cutBookTitle(book.title)}</h3>
+                         <p class="books-card-author">${cutBookAuthor(book.author)}</p>
                      </div>
                   </a>
              </li>`}).join('')}
@@ -47,6 +74,39 @@ function booksCardTemplate(data) {
 </ul>`}
 
 
+function cutBookTitle(title) {
+  if (window.innerWidth <= 767 && title.length >= 27)
+    return title
+      .substring(0, 27)
+      .toUpperCase()
+      .replace(/\s[A-Z]*$/g, '...');
+
+  if (window.innerWidth > 767 && title.length >= 19)
+    return title
+      .substring(0, 19)
+      .toUpperCase()
+      .replace(/\s[A-Z]*$/g, '...');
+
+  return title;
+}
+
+function cutBookAuthor(author) {
+  if (window.innerWidth <= 767 && author.length >= 37)
+    return author
+      .substring(0, 37)
+      .toUpperCase()
+      .replace(/\s[A-Z]*$/g, '...');
+
+  if (window.innerWidth > 767 && author.length >= 29)
+    return author
+      .substring(0, 29)
+      .toUpperCase()
+      .replace(/\s[A-Z]*$/g, '...');
+
+  return author;
+}
+
+// const homeBooksByType = document.querySelector('.home-typeBook')
 // const homeBtnEl = document.querySelectorAll('.books-btn');
 
 // const showTypeBook = async type => {
@@ -85,5 +145,3 @@ function booksCardTemplate(data) {
 //     Notiflix.Notify.failure(`Not found`);
 //   }
 // }
-
-
