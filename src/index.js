d56c0b385be3c739ad91d7b4da43ea743fe3c2b2
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2023-10-16 14:33:10 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './js/header';
+import './js/header.js';
 import './js/mobil-menu.js';
 import './js/category.js';
 import './js/bookcard.js';
 import './js/support_object.js';
 import './js/modal-shop-list.js';
-import './js/scrollup';
+import './js/scrollup.js';
 import './js/theme.js';
 
 
