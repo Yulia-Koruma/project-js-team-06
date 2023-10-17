95eebccf1bdf70b333dd83af907234b90c9fcda5
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2023-10-17 09:47:43 +0300

    fix parthials

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,8 +1,11 @@
 import './js/header';
-import "./js/shop-list.js";
 import "./js/mobil-menu.js";
+import './js/support_object.js';
+import './js/modal-shop-list';
+import "./js/shop-list.js";
 import './js/theme.js';
-import './js/support_object.js'
+import './js/scrollup.js';
+
 
 
 
