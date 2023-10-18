9a9fb825a0627180a515d0ae5a2d4e446033d060
Korobenko2023 <vivika3110@gmail.com>
2023-10-18 23:02:07 +0300

    add two links

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,7 @@
 const currentURL = window.location.href;
 
 const menuLinks = document.querySelectorAll('.header-nav-link');
+const mobLinks = document.querySelectorAll('.mob-menu-link');
 
 menuLinks.forEach(link => {
   const linkURL = link.href;
@@ -11,6 +12,19 @@ menuLinks.forEach(link => {
   }
 });
 
+mobLinks.forEach(link => {
+  const linkMobURL = link.href;
+  const mobNavText = link.querySelector('.mob-nav-text');
+  const mobSvg = document.querySelector('.mob-nav-svg');
 
 
-
+    if (currentURL === linkMobURL) {
+      link.closest('.mob-nav-item').classList.add('current-mob');
+      mobNavText.style.color = 'var(--black)';
+      mobSvg.style.fill = 'var(--black)';
+    } else {
+      link.closest('.mob-nav-item').classList.remove('current-mob');
+      mobNavText.style.color = 'var(--white)';
+      mobSvg.style.fill = 'var(--white)';
+  }
+});
