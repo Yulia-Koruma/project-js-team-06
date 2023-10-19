181169d2a3c0541a495db1a715d379c06df418a7
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2023-10-19 14:58:23 +0300

    add fix

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,9 +1,9 @@
 import './js/header';
 import "./js/mobil-menu.js";
-import './js/bookcard.js';
+// import './js/bookcard.js';
 import './js/support_object.js';
 import "./js/shop-list.js";
-import './js/modal-shop-list';
+// import './js/modal-shop-list';
 import './js/theme.js';
 import './js/scrollup.js';
 
